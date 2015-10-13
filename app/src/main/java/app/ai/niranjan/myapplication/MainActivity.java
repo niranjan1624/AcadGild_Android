09df5b4616f847e8fb9e21c69ddc650b32924ad7
niranjan1624 <niranjan.ece1@gmail.com>
2015-10-13 13:56:00 +0530

    login_sreen changed

--- app/src/main/java/app/ai/niranjan/myapplication/MainActivity.java
+++ app/src/main/java/app/ai/niranjan/myapplication/MainActivity.java
@@ -1,7 +1,8 @@
 package app.ai.niranjan.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, HideNSeek.class);
+        startActivity(intent);
     }
 
     @Override
